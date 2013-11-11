fe9d6fd7c551ec0d1d658a05b5ed5d2e404aa03b
Austin Sharp <austinsharp111@gmail.com>
2013-11-10 16:38:40 -0800

    naked triples works I think!!

--- State.cpp
+++ State.cpp
@@ -253,6 +253,9 @@ State::constraintPropagation(const rule strongest)
 			*/
 			for(int x = 0; x < 9; x++) {
 				for(int y = 0; y < 9; y++) {
+					if(board[x][y].domain.size() != 2) {
+						continue;
+					}
 					for(int i = 0; i < 9; i++) {
 						bool sameDomRow = true;
 						bool sameDomCol = true;
@@ -346,6 +349,123 @@ State::constraintPropagation(const rule strongest)
 
 		if(strongest >= NAKED_TRIPLES) {
 			// rule #4 - naked triples, k = 3
+			/*
+				for each cell 
+					check if 2 other cells in row/col/box have same domain
+					if so, record coords of the 3 and the domain. remove each domain item from all others in row/col/box.
+			*/
+			for(int x = 0; x < 9; x++) {
+				for(int y = 0; y < 9; y++) {
+					if(board[x][y].domain.size() != 3) {
+						continue;
+					}
+
+					// coords of matches
+					std::vector<int> row_matches;
+					std::vector<int> col_matches;
+
+					for(int i = 0; i < 9; i++) {
+						bool sameDomRow = true;
+						bool sameDomCol = true;
+						// check for row cells with same domain
+						for(auto v : board[x][y].domain) {
+							// make sure everything in [x][y]'s domain is in [i][y]'s domain
+							if(i != x && std::find(board[i][y].domain.begin(), board[i][y].domain.end(), v) == board[i][y].domain.end()) {
+								sameDomRow = false;
+							}
+
+							// if domain value exists in column cell's domain, it's not unique
+							if(i != y && std::find(board[x][i].domain.begin(), board[x][i].domain.end(), v) == board[x][i].domain.end()) {
+								sameDomCol = false;
+							}
+						}
+						// check if a row or col cell of matching domain was found
+						if(sameDomRow && i != x && board[i][y].domain.size() == 3) {
+							row_matches.push_back(i);
+						}
+						if(sameDomCol && i != y && board[x][i].domain.size() == 3) {
+							col_matches.push_back(i);
+						}
+					}
+
+					// if we found 2 matches, remove the domain of these 3 from the rest of the row/col
+					if(row_matches.size() == 2) {
+						// remove from other domains in row
+						for(auto v : board[x][y].domain) {
+							for(int j = 0; j < 9; j++) {
+								if(j != x && j != row_matches[0] && j != row_matches[1]) {
+									// erase this val from [j][y]'s domain
+									int sz = board[j][y].domain.size();
+									board[j][y].domain.erase(std::remove(board[j][y].domain.begin(), board[j][y].domain.end(), v), board[j][y].domain.end());
+									if(sz > board[j][y].domain.size()) {
+										applied = true;
+						
+										if(!updateDomains()) {
+											return false;
+										}
+									}
+								}
+							}
+						}
+					}
+					if(col_matches.size() == 2) {
+						// remove from other domains in row
+						for(auto v : board[x][y].domain) {
+							for(int j = 0; j < 9; j++) {
+								if(j != y && j != col_matches[0] && j != col_matches[1]) {
+									// erase this val from [x][j]'s domain
+									int sz = board[x][j].domain.size();
+									board[x][j].domain.erase(std::remove(board[x][j].domain.begin(), board[x][j].domain.end(), v), board[x][j].domain.end());
+									if(sz > board[x][j].domain.size()) {
+										applied = true;
+						
+										if(!updateDomains()) {
+											return false;
+										}
+									}
+								}
+							}
+						}
+					}
+
+
+					std::vector<std::pair<int, int>> box_matches;
+					for(auto xy : boxOtherCoords(x, y)) {
+						bool sameDomBox = true;
+						for(auto v : board[x][y].domain) {
+							// make sure everything in [x][y]'s domain is in [xy.first][xy.second]'s domain
+							if(std::find(board[xy.first][xy.second].domain.begin(), board[xy.first][xy.second].domain.end(), v) == board[xy.first][xy.second].domain.end()) {
+								sameDomBox = false;
+							}
+						}
+						if(sameDomBox && board[xy.first][xy.second].domain.size() == 3) {
+							box_matches.push_back(xy);
+						}
+					}
+
+					if(box_matches.size() == 2) {
+						// erase all values from [x][y]'s domain from boxes that aren't xy or in box_matches
+						auto cells = boxOtherCoords(x, y);
+						for(auto m : box_matches) {
+							cells.erase(std::remove(cells.begin(), cells.end(), m), cells.end());
+						}
+
+						for(auto v : board[x][y].domain) {
+							for(auto c : cells) {
+								int sz = board[c.first][c.second].domain.size();
+								board[c.first][c.second].domain.erase(std::remove(board[c.first][c.second].domain.begin(), board[c.first][c.second].domain.end(), v), board[c.first][c.second].domain.end());
+								if(sz > board[c.first][c.second].domain.size()) {
+									applied = true;
+						
+									if(!updateDomains()) {
+										return false;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
